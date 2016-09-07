4e252ea43b8c823de76f443d2025826884e53b73
Sitesfor.Church <sitesforchurch@gmail.com>
2016-09-07 16:24:02 -0500

    added gospel, leadership and belief pages

--- _airtable.js
+++ _airtable.js
@@ -119,36 +119,34 @@ var updated = false;
 // });
 
 
-// // Tested and if updated or created build json and record to file
-// if(updated) {
-    abouts('Pages').select({
-        maxRecords: 100,
-      //sort
-        sort: [{field: "title", direction: "asc"}],
-        filterByFormula: "TRUE(published)",
-      //Formula to how to get data
-      // help https://support.airtable.com/hc/en-us/articles/203255215-Formula-Field-Reference
+abouts('Other Pages').select({
+    maxRecords: 100,
+  //sort
+    sort: [{field: "title", direction: "asc"}],
+    filterByFormula: "TRUE(published)",
+  //Formula to how to get data
+  // help https://support.airtable.com/hc/en-us/articles/203255215-Formula-Field-Reference
 
-    }).eachPage(function page(records, fetchNextPage) {
+}).eachPage(function page(records, fetchNextPage) {
 
-        // This function (`page`) will get called for each page of records.
+    // This function (`page`) will get called for each page of records.
 
-        records.forEach(function(record) {
-          aboutJson.push(record._rawJson.fields);
-          
-        });
-        fetchNextPage();
-
-    }, function done(error) {
-        if (error) {
-            console.log(error);
-        }
-      jsonfile.writeFile(fileAbout, aboutJson, function (err) {
-        console.error(err)
-      });
-      console.log('abouts worked');
+    records.forEach(function(record) {
+      aboutJson.push(record._rawJson.fields);
+      
     });
-// }
+    fetchNextPage();
+
+}, function done(error) {
+    if (error) {
+        console.log(error);
+    }
+  jsonfile.writeFile(fileAbout, aboutJson, function (err) {
+    console.error(err)
+  });
+  console.log('abouts worked');
+});
+
 
 
 // blog 
@@ -612,3 +610,115 @@ var updated = false;
       });
       console.log('contact_page worked');
     });
+
+// Beliefs data
+
+var config = loadConfig().airtable;
+var jsonfile = require('jsonfile');
+var filebeliefs = '_data/beliefs.json';
+var beliefs = new Airtable({ apiKey: config.apikey }).base(config.abouts);
+var beliefsJson = [];
+var beliefsJsonTest = [];
+
+beliefs('Beliefs').select({
+    maxRecords: 100,
+    view: "Main View"
+  //sort
+    
+  //Formula to how to get data
+  // help https://support.airtable.com/hc/en-us/articles/203255215-Formula-Field-Reference
+
+}).eachPage(function page(records, fetchNextPage) {
+
+    // This function (`page`) will get called for each page of records.
+
+    records.forEach(function(record) {
+      beliefsJson.push(record._rawJson.fields);
+      
+    });
+    fetchNextPage();
+
+}, function done(error) {
+    if (error) {
+        console.log(error);
+    }
+  jsonfile.writeFile(filebeliefs, beliefsJson, function (err) {
+    console.error(err)
+  });
+  console.log('beliefs worked');
+});
+
+// Gospel data
+
+var config = loadConfig().airtable;
+var jsonfile = require('jsonfile');
+var filegospel = '_data/gospel.json';
+var gospel = new Airtable({ apiKey: config.apikey }).base(config.abouts);
+var gospelJson = [];
+var gospelJsonTest = [];
+
+gospel('Gospel').select({
+    maxRecords: 1,
+    view: "Main View"
+  //sort
+    
+  //Formula to how to get data
+  // help https://support.airtable.com/hc/en-us/articles/203255215-Formula-Field-Reference
+
+}).eachPage(function page(records, fetchNextPage) {
+
+    // This function (`page`) will get called for each page of records.
+
+    records.forEach(function(record) {
+      gospelJson.push(record._rawJson.fields);
+      
+    });
+    fetchNextPage();
+
+}, function done(error) {
+    if (error) {
+        console.log(error);
+    }
+  jsonfile.writeFile(filegospel, gospelJson, function (err) {
+    console.error(err)
+  });
+  console.log('gospel worked');
+});
+
+
+// Leadership data 
+
+var config = loadConfig().airtable;
+var jsonfile = require('jsonfile');
+var fileleadership = '_data/leadership.json';
+var leadership = new Airtable({ apiKey: config.apikey }).base(config.abouts);
+var leadershipJson = [];
+var leadershipJsonTest = [];
+
+leadership('Leadership').select({
+    maxRecords: 100,
+    view: "Main View"
+  //sort
+    
+  //Formula to how to get data
+  // help https://support.airtable.com/hc/en-us/articles/203255215-Formula-Field-Reference
+
+}).eachPage(function page(records, fetchNextPage) {
+
+    // This function (`page`) will get called for each page of records.
+
+    records.forEach(function(record) {
+      leadershipJson.push(record._rawJson.fields);
+      
+    });
+    fetchNextPage();
+
+}, function done(error) {
+    if (error) {
+        console.log(error);
+    }
+  jsonfile.writeFile(fileleadership, leadershipJson, function (err) {
+    console.error(err)
+  });
+  console.log('leadership worked');
+});
