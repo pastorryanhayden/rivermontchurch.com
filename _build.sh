0e4f5f3a2f403fa5220e589af4de8a1196148401
Sitesfor.Church <sitesforchurch@gmail.com>
2016-09-07 10:57:42 -0500

    Initial Commit
    This is the New England theme with some new tweaks.

---
+++ _build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+path="/Users/ryanhayden/github/communitybaptistchurch.net/";
+git_path="https://github.com/pastorryanhayden/communitybaptistchurch.net.git";
+need_build=false;
+#!/usr/bin/env bash
+
+echo "Navigate to web root: $path";
+
+cd $path;
+
+echo "Update repo ...";
+git pull $git_path;
+
+echo "Get Data";
+node ./_airtable.js;
+echo "Build Jekyll";
+jekyll build
+echo "Compile SASS";
+node-sass _css/app.scss _site/assets/css/app.css;
+echo "Upload via glynn";
+glynn;
+# echo "Sync";
+# osascript upload.scpt;
+# echo -e '\n Complete all scripts';
